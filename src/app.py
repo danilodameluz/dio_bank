bc93a357cd9dc64ac940226656e0bf52c270f092
Danilo Dameluz - MLHS <danilo.dameluz@mlhs.com.br>
2025-05-22 16:18:22 -0300

    Inserindo os primeiros dados e visualizando

--- src/app.py
+++ src/app.py
@@ -49,7 +49,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
+        SQLALCHEMY_DATABASE_URI="sqlite:///blog.sqlite",
     )
 
     if test_config is None:
@@ -64,5 +64,10 @@ def create_app(test_config=None):
 
     # initialize extensions
     db.init_app(app)
+
+    # register blueprints
+    from src.controlers import user
+
+    app.register_blueprint(user.app)
 
     return app
