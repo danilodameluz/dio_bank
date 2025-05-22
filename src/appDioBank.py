bfdddedf7ade2e07cf6f8005ed50721a02b58012
Danilo Dameluz - MLHS <danilo.dameluz@mlhs.com.br>
2025-05-22 10:43:05 -0300

    Criação de Banco de Dados e Mapeamento de colunas, usando SQLAlchemy

--- src/appDioBank.py
+++ src/appDioBank.py
@@ -1,4 +1,47 @@
-from flask import Flask
+from datetime import datetime
+import click
+import sqlalchemy as sa
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return (
+            f"Post(id={self.id!r}, title={self.title!r}, authorid={self.author_id!r})"
+        )
+
+
+@click.command("init-db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo("Initialized the database.")
 
 
 def create_app(test_config=None):
@@ -6,7 +49,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE="diobank.sqlite",
+        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
     )
 
     if test_config is None:
@@ -16,8 +59,10 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    from . import db
+    # register cli comands
+    app.cli.add_command(init_db_command)
 
+    # initialize extensions
     db.init_app(app)
 
     return app
